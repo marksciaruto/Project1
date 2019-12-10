0d02f6437b4e30a5a025c4a33c6527c1cfbd44fe
msciaruto <marksciaruto@gmail.com>
2019-12-10 16:01:29 -0500

    Update form onsubmit js

---
+++ index.js
@@ -0,0 +1,9 @@
+const  marksAwesomeSubmitHandler = () => {
+    console.log("The form has been submitted")
+}
+
+/*<form onsubmit="onsubmit()">
+    Enter name: <input type="text">
+    <input type="submit">
+    </form>
+*/
